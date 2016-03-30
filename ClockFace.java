d143e6b27445ebc4256921320b9e6c3d27f87edb
Bernard-Rockson <bernard.rockson.bb@gmail.com>
2016-03-30 09:49:39 -0700

    ClockFace

--- ClockFace.java
+++ ClockFace.java
@@ -5,33 +5,38 @@
  */
 package assignment3_10533515;
 
+import javax.swing.JFrame;
+import java.awt.*;
+
 /**
  *
  * @author NANA
  */
- import java.awt.*;
-import javax.swing.JFrame;
+public class ClockFace extends Canvas{
+    public void paint(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 500, 500);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Consolas", Font.BOLD, 20));
+        g.drawOval(75, 75, 350, 350);
+        g.drawString("12", 240, 100);
+        g.drawString("3", 400, 250);
+        g.drawString("6", 240, 400);
+        g.drawString("9", 80, 250);
+        
+        g.drawLine(100, 250, 250, 250);
+        g.drawLine(150, 300, 250, 250);
 
-public class ClockFace extends Canvas
-{
-	public void paint( Graphics g )
-	{
-          g.drawOval(200,150,310,300);
-          g.setColor(Color.black);
-          g.fillOval(345, 295, 10, 10);
-          g.drawLine(350, 300, 230, 300);
-          g.drawLine(350, 300, 350, 360);
-            
-        }
 
-public static void main( String[] args )
-	{
-		// You can change the title or size here if you want.
-		JFrame win = new JFrame("ClockFace");
-		win.setSize(800,600);
-		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ClockFace canvas = new ClockFace();
-		win.add( canvas );
-		win.setVisible(true);
-	}
+    }
+    public static void main(String[] args) {
+        JFrame win=new JFrame("Clock Face");
+        win.setSize(500, 500);
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ClockFace canvas=new ClockFace();
+        win.add( canvas );
+        win.setVisible(true);
+    }
+    
 }
+
